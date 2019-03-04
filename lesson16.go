d572b5f9f8d0b1f710d3b60f662473c5004224b7
Tsuyoshi Hirosawa <dmwtpdmwtp24@gmail.com>
2019-03-05 01:18:41 +0900

    lesson16

---
+++ lesson16.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := make([]int, 3, 5)
+	fmt.Printf("len=%d\n cap=%d\n value=%v\n", len(n), cap(n), n)
+	n = append(n, 0, 0)
+	fmt.Printf("len=%d\n cap=%d\n value=%v\n", len(n), cap(n), n)
+	n = append(n, 1, 2, 3, 4, 5, 6, 7)
+	fmt.Printf("len=%d\n cap=%d\n value=%v\n", len(n), cap(n), n)
+
+	b := make([]int, 0)
+	var c []int
+	fmt.Printf("len=%d\n cap=%d\n value=%v\n", len(b), cap(b), b)
+	fmt.Printf("len=%d\n cap=%d\n value=%v\n", len(c), cap(c), c)
+
+	// c = make([]int, 5)
+	c = make([]int, 0, 5)
+	for i := 0; i < 5; i++ {
+		c = append(c, i)
+		fmt.Println(c)
+	}
+}
